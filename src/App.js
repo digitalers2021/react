63145ae648540e1548903bcd3fa5476c7535953e
nuxion <nuxion@gmail.com>
2021-09-20 21:02:09 -0300

    clase 3: react-router agregado

--- src/App.js
+++ src/App.js
@@ -4,18 +4,59 @@ import Navbar from './components/Navbar'
 import Logo2 from './components/Logo2'
 import Contador from './components/Contador'
 import Contador2 from './components/Contador2'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 const msg = "Hola mundo!";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
+    <Router>
+      <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cursos">Cursos</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/cursos">
+          <Cursos></Cursos>
+        </Route>
+       <Route path="/about">
+          <About></About>
+        </Route>
+        <Route path="/">
+          <Home></Home>
+        </Route>
+
+      </Switch>
+      </div>
+    </Router>
+    
+  );
+}
+
+
+function Home(){
+  return <div className="App">
       <Logo2 msg={msg}></Logo2>
       <header className="App-header">
       <Logo></Logo>
-      <Contador></Contador>
-      <Contador></Contador>
+      <Contador nombre="incrementar"></Contador>
+      <Contador nombre="incrementar 2"></Contador>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -32,7 +73,29 @@ function App() {
         </a>
       </header>
     </div>
-  );
+}
+
+function About() {
+  return <div class="about">
+    <h1>About</h1>
+    <p>Esta es un ejercicio para ver el router en react</p>
+
+  </div>
+
+}
+
+
+function Cursos() {
+  return <div class="cursos">
+    <h1>Cursos</h1>
+    <ul>
+      <li>Python</li>
+      <li>React</li>
+
+    </ul>
+
+  </div>
+
 }
 
 export default App;
--- src/components/Contador.js
+++ src/components/Contador.js
@@ -3,14 +3,14 @@ import React from 'react';
 export default class Contador extends React.Component {
   constructor(props){
     super(props)
-    this.state = {contador: 0}
+    this.state = { contador: 0 }
 
   }
   render(){
     return (
       <div class="contador">
         <button onClick={() => this.setState({ contador: this.state.contador + 1 })}>
-          Incrementar
+          { this.props.nombre }
         </button>
         { this.state.contador }
 
